4112aab19a7424bfba36bef77892ccba665323ad
eawhite00 <eawhite00@gmail.com>
2019-09-19 23:48:52 -0700

    Added final stylings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-
-
+//set up our global variables
 var targetScore = 0;
 var currentScore = 0;
 var lastScore = 0
@@ -12,7 +11,7 @@ var losses = 0;
 
 
 
-
+//This function sets up the game, resetting values / displays
 function setUp(){
     currentScore = 0;
     lastScore = 0;
@@ -28,12 +27,14 @@ function setUp(){
 
 }
 
+//This is a part of setup that I made its own function for readability- it sets the values for each crytsal.
 function setCrystalScores() {
     for (i = 0; i < crystalScores.length; i++){
         crystalScores[i] = Math.floor( Math.random() * 12 ) + 1;
     }
 }
 
+//Here's where we handle adding a new score, including checking for / tracking wins/losses
 function addScore(crystalNumber){
 
     lastScore = crystalScores[crystalNumber];
@@ -53,7 +54,7 @@ function addScore(crystalNumber){
 
 }
 
-
+//Here's the button click handlers- all it does is pass the crystal number as an index to the addScore function.
 $("#box1").on("click", function(){
     addScore(0);
 });
@@ -70,8 +71,6 @@ $("#box4").on("click", function(){
     addScore(3);
 });
 
-
-
-
+//Here's our initial setup to get the game ready.
 setUp();
-console.log(crystalScores);
+
